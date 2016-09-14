8ab988dcbfde6c167467256ff3ef27dd0bfbf229
clevlehi <clevlehi@greeny.cs.tlu.ee>
2016-09-14 08:54:00 +0300

    first lines of code

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<?php
+	//muutuja ette k2ib $
+	//rea l6ppu k2ib ;
+
+	$firstname="Cleven";
+	$lastname="Lehispuu";
+	//echo $firstname;
+	//echo $lastname;
+	//punkt liidab stringe
+	echo $firstname." ".$lastname
+?>
